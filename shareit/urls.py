b239d949dff99f60bdd074d5cbfc345aa2b9e920
zhouye <zhouye01@gmail.com>
2013-10-06 20:47:55 +0800

    Local server now runs fine, and CSS is a holy piece of shit

--- shareit/urls.py
+++ shareit/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from shareit.admin import admin
@@ -14,4 +16,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
++ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
