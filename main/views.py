600185ccd2807c8d230fe10cbaf870bf29c0bddb
Wandsea <wandsea@gmail.com>
2013-10-15 20:19:15 +0800

    account login & reg.

--- main/views.py
+++ main/views.py
@@ -7,16 +7,16 @@ from main.models import Document
 from main.forms import DocumentForm
 
 def index(request):
-    template = loader.get_template('index.html')
+    template = loader.get_template('main/index.html')
     documents = Document.objects.all()
     return render_to_response(
-        'index.html',
+        'main/index.html',
         {'documents': documents},
         context_instance=RequestContext(request)
     )
 
 def upload(request):
-    template = loader.get_template('upload.html')
+    template = loader.get_template('main/upload.html')
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
@@ -26,7 +26,7 @@ def upload(request):
     else:
         form = DocumentForm() # A empty, unbound form
     return render_to_response(
-        'upload.html',
+        'main/upload.html',
         {'form': form},
         context_instance=RequestContext(request)
     )
--- shareit/urls.py
+++ shareit/urls.py
@@ -2,19 +2,17 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
 from shareit.admin import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'main.views.index', name = "index"),
     url(r'^upload/', 'main.views.upload', name = "upload"),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 
-    # Uncomment the next line to enable the admin:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
 + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
